5d045bdc511bdfbfaa1c9b4f542130e03ed94118
Ksrushti08 <115167692+Ksrushti08@users.noreply.github.com>
2024-05-05 14:18:16 +0530

    angularts

---
+++ angular.ts
@@ -0,0 +1,10 @@
+title="Register Forms"
+displayname=""
+displayaddress=""
+displayname=""
+displayname=""
+getvalue(name:string,address:string,contact:string,email:string){
+this.displayname=name;
+  this.dispalyaddress=address;
+  this.display
+}
